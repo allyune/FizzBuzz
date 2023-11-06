10b821c6d8d17534c94be184935a1e00af0fcbfb
Polina Krasnianska <p.krasniansky@gmail.com>
2023-11-06 16:17:55 +0100

    test (additional strings) Add test cases for additional FizzBuzz strings

--- FizzBuzzGitTest/FizzBuzzTests.cs
+++ FizzBuzzGitTest/FizzBuzzTests.cs
@@ -12,6 +12,9 @@ public class Tests
     [TestCase(3, "Fizz")]
     [TestCase(5, "Buzz")]
     [TestCase(15, "FizzBuzz")]
+    [TestCase(81, "81")]
+    [TestCase(7, "Whizz")]
+    [TestCase(11, "Bang")]
     public void FizzBuzzTest(int num, string expectedResult)
     {
         string realResult = FizzBuzz.GenerateFizzBuzz(num);
