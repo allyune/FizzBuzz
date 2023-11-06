2ff2154c4164b6f53653d8f7f4f7824ebb34c5b9
Polina Krasnianska <p.krasniansky@gmail.com>
2023-11-06 20:08:24 +0100

    feature (additional strings): Call FIZZBUZZ by creating an instance of the class

--- FizzBuzzGitTest/FizzBuzzTests.cs
+++ FizzBuzzGitTest/FizzBuzzTests.cs
@@ -20,4 +20,16 @@ public class Tests
         string realResult = FizzBuzz.GenerateFizzBuzz(num);
         Assert.That(realResult, Is.EqualTo(expectedResult));
     }
+
+    [TestCase(3, "Hello")]
+    [TestCase(5, "World")]
+    public void FizzBuzzCustomStringsTest(int num, string expectedResult)
+    {
+        FizzBuzzParameters parameters = new FizzBuzzParameters();
+        parameters.divisibleByThreeStr = "Hello";
+        parameters.divisibleByFiveStr = "World";
+        FizzBuzz fizzBuzz = new FizzBuzz(parameters);
+        string realResult = fizzBuzz.GenerateFizzBuzz(num);
+        Assert.That(realResult, Is.EqualTo(expectedResult));
+    }
 }
