b4842bc8ea65b780d0ccad7b50d5d5431b3bac7c
Polina Krasnianska <p.krasniansky@gmail.com>
2023-11-06 20:02:37 +0100

    feature (additional strings): Use FizzBuzzParameters to pass custom strings to the class

--- FizzBuzzGitTest/FizzBuzzTests.cs
+++ FizzBuzzGitTest/FizzBuzzTests.cs
@@ -12,12 +12,14 @@ public class Tests
     [TestCase(3, "Fizz")]
     [TestCase(5, "Buzz")]
     [TestCase(15, "FizzBuzz")]
-    [TestCase(81, "81")]
+    [TestCase(83, "83")]
     [TestCase(7, "Whizz")]
     [TestCase(11, "Bang")]
     public void FizzBuzzTest(int num, string expectedResult)
     {
-        string realResult = FizzBuzz.GenerateFizzBuzz(num);
+        FizzBuzzParameters parameters = new FizzBuzzParameters();
+        FizzBuzz fizzBuzz = new FizzBuzz(parameters);
+        string realResult = fizzBuzz.GenerateFizzBuzz(num);
         Assert.That(realResult, Is.EqualTo(expectedResult));
     }
 }
